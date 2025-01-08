35837ea12d3280714306e78291e13bb29dcc1c58
pseudoureka <messiah7179@gmail.com>
2025-01-08 20:41:56 +0900

    then 메소드

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-function sayHello(name) {
-  console.log(`Hello ${name}!`);
+function getRandomElement(arr) {
+  const randomIdx = Math.floor(Math.random() * arr.length);
+  return arr[randomIdx];
 }
 
-console.log("시작");
+console.log("메뉴 고르는 중...");
 
-setTimeout(sayHello, 3000, "Codeit!");
-
-console.log("끝");
+fetch("https://learn.codeit.kr/api/menus")
+  .then((response) => response.json())
+  .then((menus) => {
+    const randomMenu = getRandomElement(menus);
+    console.log(`오늘의 랜덤 메뉴는 ${randomMenu.name}입니다!`);
+  });
